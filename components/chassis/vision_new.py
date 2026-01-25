de61cdd4c90ca8ed76e1c3e7fb86dbfe13b7cae7
KhushiJain14908 <khushijain14908@gmail.com>
2026-01-25 15:21:12 -0800

    websocket connected and visible on driver station (though data values not updated from limelight yet)
    Co-authored-by: ansh-pi <ansh-pi@users.noreply.github.com>
    Co-authored-by: elterbug <elterbug@users.noreply.github.com>
    Co-authored-by: elaine-z905 <elaine-z905@users.noreply.github.com>

--- components/chassis/vision_new.py
+++ components/chassis/vision_new.py
@@ -6,63 +6,50 @@ from websocket import create_connection, WebSocketException, WebSocketApp
 
 class Vision:
     def __init__ (self):
-        l1_address = "10.6.68.11"
-        self.l1 = limelight.Limelight(l1_address)
+        self.l1_address = "10.6.68.11"
+        self.l1 = limelight.Limelight(self.l1_address)
         self.l1.enable_websocket()
-        # self.websocketConnected = False
-        # self.websocketAttemptDelay = 0
-        self.ws = WebSocketApp("ws://10.6.68.11:5806",
-                              on_open=self.on_open,
-                              on_message=self.on_message,
-                              on_error=self.on_error,
-                              on_close=self.on_close)
-        self.ws.run_forever()
-        
+        self.websocketConnected = False
+        self.websocketAttemptDelay = 0
+        self.ws = None  
+        self.m = "none"
 
-    '''def connectWebsocket(self):
+    def connectWebsocket(self):
+        self.m = "started"
         try:
-            self.ws = create_connection("ws://10.6.68.11:5806")
+            self.ws = create_connection(f"ws://{self.l1_address}:5806")
             self.websocketConnected = True
-            print("websocket connected")
+            print("Websocket connected")
+            self.m = "connected"
         except Exception as e:
             self.websocketConnected = False
-            print("websocket failed to connect connected")
+            print("Websocket failed to connect: ")
             print(e)
-    '''
-
-    def stream_results(self):
-        
-        """
-        print("Connected:", self.l1.websocket_connected)
-        result = self.l1.get_latest_results()
-        parsed_result = limelightresults.parse_results(result)
-        """    
-
-    def execute(self)->None:
-        """
+            self.m = "fail"
+        if self.m == "started":
+            self.m = "nope"
+    
+    def execute(self) -> None:
         if self.websocketAttemptDelay <= 0: 
             if not self.websocketConnected: 
+                self.websocketAttemptDelay = 20
                 self.connectWebsocket()
             else:
                 self.websocketAttemptDelay -= 1
-        """
+        print(self.m)
+        if self.ws is not None:
+            if self.ws.connected:
+                self.receiveMessage(self.ws.recv())
         
+    def receiveMessage(self,_data) -> None:
+        if _data is None:
+            return
+        data = json.loads(_data)
+        print(data)
+        # if data.get("v") == 1:
+        #     fiducial = data.get("Fiducial", {})
+        #     print("Fiducial Data: ", fiducial)
+        #     print("botpose_orb_wpiblue: ", data.get("botpose_orb_wpiblue"))
+        # get location based off of data and do things
 
-    # def stream_results(self):
-    #     packet = self.l1.get_latest_results()
-    #     print("valid targets: ", packet.validity, ", pipelineIndex: ", packet.pipeline_id,", Targeting Latency: ", packet.targeting_latency)
-    #     if packet is not None:
-    #         for tag in packet.fiducialResults:
-    #             print(tag.robot_pose_target_space, tag.fiducial_id)
-    #         time.sleep(1)
-    #     else:
-    #         print("parsed_result is none")
-
-    def on_message(ws, message):
-        print(f"Received: {message}")
-    def on_open(ws):
-        print("Webcsocket opened")
-    def on_error(ws, error):
-        print(f"Error: {error}")
-    def on_close(ws, close_status_code, close_msg):
-        print("websocket closed :(")
+    
