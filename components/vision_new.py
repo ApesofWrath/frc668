9f95d948ba0e42ce3a0093174ab927c0e2758b71
Ansh <anshp6068@gmail.com>
2026-01-30 17:49:23 -0800

    Co-authored-by: yuuuvraaj-singh <yuuuvraaj-singh@users.noreply.github.com> Co-authored-by: elterbug <elterbug@users.noreply.github.com> Co-authored-by: chikiupgaming <chikiupgaming@users.noreply.github.com> Co-authored-by: elaine-z905 <elaine-z905@users.noreply.github.com> Co-authored-by: Khushijain14908 <Khushijain14908@users.noreply.github.com>

--- components/vision_new.py
+++ components/vision_new.py
@@ -1,56 +1,29 @@
-import limelight 
-import limelightresults
-import json
-import time 
-from websocket import create_connection, WebSocketException, WebSocketApp
+import ntcore
+import constants
+from magicbot import feedback
+from components.limelight import LimelightHelpers, PoseEstimate 
+from phoenix6 import hardware
 
 class Vision:
-    def __init__ (self):
-        self.l1_address = "10.6.68.11"
-        self.l1 = limelight.Limelight(self.l1_address)
-        self.l1.pipeline_switch(0)
-        self.l1.enable_websocket()
-        self.websocketConnected = False
-        self.websocketAttemptDelay = 0
-        self.ws = None  
-        self.m = "none"
 
-    def connectWebsocket(self):
-        self.m = "started"
-        try:
-            self.ws = create_connection(f"ws://{self.l1_address}:5806")
-            self.websocketConnected = True
-            print("Websocket connected")
-            self.m = "connected"
-        except Exception as e:
-            self.websocketConnected = False
-            print("Websocket failed to connect: ")
-            print(e)
-            self.m = "fail"
-        if self.m == "started":
-            self.m = "nope"
+    def __init__(self):
+        self.l1 = "limelight-one"
+        table = ntcore.NetworkTableInstance.getDefault().getTable(self.l1)
+        self.newTable = table.getDoubleArrayTopic("botpose_orb_wpiblue").subscribe([0,0,0,0,0,0])
+        self.pigeon = hardware.Pigeon2(constants.PIGEON_ID, "rio")
     
     def execute(self) -> None:
-        if self.websocketAttemptDelay <= 0: 
-            if not self.websocketConnected: 
-                self.websocketAttemptDelay = 20
-                self.connectWebsocket()
-            else:
-                self.websocketAttemptDelay -= 1
-        print(self.m)
-        if self.ws is not None:
-            if self.ws.connected:
-                self.receiveMessage(self.ws.recv())
-        
-    def receiveMessage(self,_data) -> None:
-        if _data is None:
-            return
-        data = json.loads(_data)
-        print(data)
-        if data.get("v") == 1:
-            fiducial = data.get("Fiducial", {})
-            print("Fiducial Data: ", fiducial)
-            print("botpose_orb_wpiblue: ", data.get("botpose_orb_wpiblue"))
-        # get location based off of data and do things
+        pass 
 
+    @feedback
+    def get_botpose_orb_wpiblue(self):
+        return self.newTable.get()
     
+    @feedback
+    def get_pose_estimate(self):
+        LimelightHelpers.set_robot_orientation(self.l1, self.pigeon.get_yaw(), 0, self.pigeon.get_pitch(), 0, self.pigeon.get_roll(), 0)
+        #TODO: fill in yaw, pitch, and roll rates 
+        mega_tag2 = LimelightHelpers.get_botpose_estimate_wpiblue_megatag2(self.l1)
+        if mega_tag2.tag_count > 0:
+            return mega_tag2
+        return None
