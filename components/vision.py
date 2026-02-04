5767b1dd6907a20a6c671d39c64d7d4912cc3330
Ansh <anshp6068@gmail.com>
2026-02-04 02:00:01 -0800

    Refactor Vision class for alliance handling and pose estimation

--- components/vision.py
+++ components/vision.py
@@ -3,10 +3,12 @@ import constants
 from magicbot import feedback
 from components.limelight import LimelightHelpers, PoseEstimate 
 from phoenix6 import hardware
+from phoenix6.swerve import swerve_drivetrain
+from phoenix6 import units
 import math
 from typing import Any
 from wpilib import DriverStation
-from phoenix6.swerve import swerve_drivetrain
+
 
 class Vision:
 
@@ -48,39 +50,44 @@ class Vision:
 
     
     def start_match (self):
-        if DriverStation.getAlliance() == DriverStation.Alliance.kBlue:
-            self.pigeon.set_yaw(0)
-        elif DriverStation.getAlliance() == DriverStation.Alliance.kRed:
-            self.pigeon.set_yaw(180)
+        alliance = DriverStation.getAlliance()
+        if alliance == DriverStation.Alliance.kBlue:
+            self.pigeon.set_yaw(0, 0.1)
+        elif alliance == DriverStation.Alliance.kRed:
+            self.pigeon.set_yaw(180, 0.1)
+        else:
+            self.pigeon.set_yaw(0,0.1)
+            print("Warning: Alliance not found at start: Defaulting to Blue (0 degrees)")
 
-    def get_pose_estimate_m2_v2(self,limelight_name):
+    def get_pose_estimate_m2(self,limelight_name):
         orientation = self.pigeon.getRotation3d()
         LimelightHelpers.set_robot_orientation(
             limelight_name,
-            orientation.Z()*180.0/math.pi, 0,
-            orientation.Y()*180.0/math.pi, 0,
-            orientation.X()*180.0/math.pi, 0
+            math.degrees(orientation.Z()), 0,
+            math.degrees(orientation.Y()), 0,
+            math.degrees(orientation.X()), 0
         )
-        mega_tag2 = LimelightHelpers.get_botpose_estimate_wpiblue_megatag2(limelight_name)
-        return mega_tag2
+        self.mega_tag2 = LimelightHelpers.get_botpose_estimate_wpiblue_megatag2(limelight_name)
+        return self.mega_tag2
 
     def field_location(self):
-        est = self.get_pose_estimate_m2_v2(self.l1)
-        if est is None or est.tag_count == 0:
+        if self.mega_tag2 is None or self.mega_tag2.tag_count == 0:
             return
         swerve_drivetrain.addVisionMeasurement(
-            est.pose,
-            est.timestamp,  
+            self.mega_tag2.pose,
+            self.mega_tag2.timestamp_seconds * units.second,
+            [1.0, 1.0, 9999999.0]
         )
+        return swerve_drivetrain.get_state().pose
 
-@feedback
-def get_orientation(self):
-    o = self.pigeon.getRotation3d()
-    return [
-        math.degrees(o.X()),
-        math.degrees(o.Y()),
-        math.degrees(o.Z())
-    ]
+    @feedback
+    def get_orientation(self):
+        o = self.pigeon.getRotation3d()
+        return [
+            math.degrees(o.X()),
+            math.degrees(o.Y()),
+            math.degrees(o.Z())
+        ]
 
 
     # def get_pose_estimate_m2(self, limelight_name) -> list[float]:
