bae21ae1170aadb45802ca916ecb3507d83f0d4a
Sanjay Narayanan <sanjaynarayanan94@gmail.com>
2026-02-08 12:52:50 -0800

    Fix method names and units in some components (#9)
    The Hopper and Indexer components were using wrong method names for the
    on_enable and on_disable MagicComponent methods. They were both also
    using the wrong unit for the motor speed. This change fixes both issues.

--- subsystem/shooter/indexer.py
+++ subsystem/shooter/indexer.py
@@ -16,30 +16,37 @@ class Indexer:
         This method is called after createObjects has been called in the main
         robot class, and after all components have been created.
         """
-        self._motor_speed_rps = 0.0
+        # A value in the range [0, 1], representing the duty cycle.
+        self._motor_speed: float = 0.0
 
     def execute(self) -> None:
         """Command the motors to the current speed.
 
         This method is called at the end of the control loop.
         """
-        self.indexer_bottom_motor.set(self._motor_speed_rps)
-        self.indexer_top_motor.set(-self._motor_speed_rps)
+        self.indexer_bottom_motor.set(self._motor_speed)
+        self.indexer_top_motor.set(-self._motor_speed)
 
-    def onEnable(self) -> None:
+    def on_enable(self) -> None:
         """Reset to a "safe" state when the robot is enabled.
 
         This method is called when the robot enters autonomous, teleoperated, or
         test mode.
         """
-        self._motor_speed_rps = 0.0
+        self._motor_speed = 0.0
 
-    def onDisable(self) -> None:
+    def on_disable(self) -> None:
         """Reset state when the robot is disabled.
 
         This method is called when the robot enters disabled mode.
         """
-        self._motor_speed_rps = 0.0
+        self._motor_speed = 0.0
 
-    def setMotorSpeedRps(self, rotations_per_second: float) -> None:
-        self._motor_speed_rps = rotations_per_second
+    def setMotorSpeed(self, speed: float) -> None:
+        """Set the speed of the indexer motors.
+
+        Args:
+            speed: A value in the range [0, 1] representing the duty cycle to
+                apply to the motor.
+        """
+        self._motor_speed = speed
