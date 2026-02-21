aad0a1383073f5dd1164b40f1041c2e1b7b60148
Abigail J <abigail.j@apesofwrath668.org>
2026-02-21 11:20:42 -0800

    started working on a test autonomous routine that makes the robot spin

---
+++ autonomous/auton_test.py
@@ -0,0 +1,27 @@
+from magicbot import AutonomousStateMachine, state, timed_state
+from subsystem import drivetrain
+from phoenix6 import swerve
+
+class Auton_Test(AutonomousStateMachine):
+    MODE_NAME = "Test"
+    DEFAULT = True
+
+    drivetrain: drivetrain.Drivetrain
+    drive_request: swerve.requests.FieldCentric
+
+    @timed_state(first=True, duration=2, next_state="stop")
+    def spin(self):
+        self.drivetrain.set_control(
+            self.drive_request.with_velocity_x(0)
+            .with_velocity_y(0)
+            .with_rotational_rate(0.6)
+        )
+
+    @state
+    def stop(self):
+        self.drivetrain.set_control(
+            self.drive_request.with_velocity_x(0)
+            .with_velocity_y(0)
+            .with_rotational_rate(0)
+        )
+        self.done()
