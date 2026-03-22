aac5baf02bce6cc927d27240bb60d4574d813387
elterbug <kyle.t@apesofwrath668.org>
2026-03-22 15:26:22 -0700

    fixed  a few things, need to test

--- subsystem/shooter/allience_tracker.py
+++ subsystem/shooter/allience_tracker.py
@@ -16,6 +16,7 @@ BLUE_ALLIENCE_END_X = 5.05
 RED_ALLIENCE_START_X = 16.5
 RED_ALLIENCE_END_X = 11.5
 FIELD_HEIGHT = 8.05
+EDGE_OFFSET = 0.75
 
 SHOOT_BACK_HOOD_ANGLE_DEG = 40
 SHOOT_BACK_FLYWHEEL_RPS = 20 #TODO: find what this
@@ -55,13 +56,13 @@ class AllienceTracker:
         # An approxamation of the turret position, it doesn't really matter that much if it's off a bit
         self._turret_field_pose: geometry.Pose2d = robot_pose
 
-        in_neutral_zone = (self._turret_field_pose.X() >= BLUE_ALLIENCE_START_X 
-                          and self._turret_field_pose.X() <= RED_ALLIENCE_START_X)
-        in_enemy_zone = (self._turret_field_pose.X() < BLUE_ALLIENCE_START_X
+        in_neutral_zone = (self._turret_field_pose.X() >= BLUE_ALLIENCE_END_X 
+                          and self._turret_field_pose.X() <= RED_ALLIENCE_END_X)
+        in_opponent_zone = (self._turret_field_pose.X() < BLUE_ALLIENCE_END_X
                          if self.alliance_fetcher.getAlliance() == wpilib.DriverStation.Alliance.kRed else
-                         self._turret_field_pose.X() > RED_ALLIENCE_START_X)
+                         self._turret_field_pose.X() > RED_ALLIENCE_END_X)
 
-        if self._enabled and (in_neutral_zone or in_enemy_zone):
+        if self._enabled and (in_neutral_zone or in_opponent_zone):
             if(self._turret_field_pose.Y() > FIELD_HEIGHT/2): #TODO flip for both alliances
                 self._target_turret_angle_degrees = self._computeTargetTurretAngleDegrees(-10) #TODO: check that this is the right way (it should be)
             else:
@@ -76,14 +77,14 @@ class AllienceTracker:
 
 
     def _computeTargetTurretAngleDegrees(
-        self, offset_degrees: phoenix6.units.degree
+        self, top_half
     ) -> phoenix6.units.degree:
         """Returns the target angle of the turret."""
         alliance_pos = (
             geometry.Translation2d(RED_ALLIENCE_START_X 
                                    if self.alliance_fetcher.getAlliance() == wpilib.DriverStation.Alliance.kRed else 
-                                   BLUE_ALLIENCE_START_X, 
-                                   self._turret_field_pose.Y())
+                                   BLUE_ALLIENCE_START_X,
+                                   (top_half*FIELD_HEIGHT)+((-1 if top_half else 1)*EDGE_OFFSET))
         )
 
         # Vector from center of turret to the alllience.
@@ -92,17 +93,18 @@ class AllienceTracker:
         )
         target_angle_degrees = (
             turret_to_allience.angle() - self._turret_field_pose.rotation()
-        ).degrees() + offset_degrees #TODO swap offset to just aim at the corner
+        ).degrees()
 
         return max(
             self.robot_constants.shooter.turret.min_angle,
             min(self.robot_constants.shooter.turret.max_angle,target_angle_degrees),
         )
     
+    @magicbot.feedback
     def getInAllienceZone(self) -> bool:
-        return (self._turret_field_pose.X() > BLUE_ALLIENCE_START_X
+        return (self._turret_field_pose.X() < BLUE_ALLIENCE_END_X
                 if self.alliance_fetcher.getAlliance() == wpilib.DriverStation.Alliance.kBlue else
-                self._turret_field_pose.X() < RED_ALLIENCE_START_X)
+                self._turret_field_pose.X() > RED_ALLIENCE_END_X)
 
     def setEnabled(self, enabled):
         self._enabled = enabled
@@ -111,17 +113,17 @@ class AllienceTracker:
         self._track_speed = speed
         self._enabled = position
 
-    # @magicbot.feedback
+    @magicbot.feedback
     def get_target_turret_angle_degrees(self) -> phoenix6.units.degree:
         """Returns the target angle for the turret to track."""
         return self._target_turret_angle_degrees
 
-    # @magicbot.feedback
+    @magicbot.feedback
     def get_target_hood_angle_degrees(self) -> phoenix6.units.degree:
         """Returns the target angle for the hood to track."""
         return self._target_hood_angle_degrees
 
-    # @magicbot.feedback
+    @magicbot.feedback
     def get_target_flywheel_speed_rps(
         self,
     ) -> phoenix6.units.rotations_per_second:
