71d6c8b38d2be5b119e183e648368e205ac2674b
KhushiJain14908 <khushijain14908@gmail.com>
2026-03-28 15:05:41 -0700

    created nz/depot auton that doesn't go back to neutral zone at end so that we have enough time to shoot all balls

---
+++ autonomous/neutral_zone_depot_shoot_starting_depot_side_no_nz_parking.py
@@ -0,0 +1,15 @@
+from autonomous import auto_base
+
+
+class NeutralZoneDepotShootStartingDepotSide(auto_base.AutoBase):
+    """
+    1. Start on the depot-side bump.
+    2. Drive over bump into the neutral zone, do 2 intake sweeps.
+    3. Drive over bump back into alliance zone, go to depot. Begin shooting.
+    4. Do 1 intake sweep over depot (outward).
+    5. Drive towards hub, allowing for time to finish shooting fuel.
+    """
+
+    MODE_NAME = "Neutral Zone and Depot Intake Shoot Starting Depot Side, NO NZ Parking"
+    TRAJECTORY_NAME = "neutral_zone_depot_shoot_starting_depot_side_no_nz_parking"
+    SHOOT_ON_THE_MOVE = True
