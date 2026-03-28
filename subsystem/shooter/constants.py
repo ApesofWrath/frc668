9ca01d5e9b1e64b63da0a004cf02bbb24dc6f334
Sanjay Narayanan <sanjaynarayanan94@gmail.com>
2026-03-27 23:26:15 -0700

    Increase flywheel coast speed to 15 rps

--- subsystem/shooter/constants.py
+++ subsystem/shooter/constants.py
@@ -79,7 +79,7 @@ class FlywheelConstants:
     k_p: float = 0.0
     k_i: float = 0.0
     k_d: float = 0.0
-    default_speed_rps: units.rotations_per_second = 1.0
+    default_speed_rps: units.rotations_per_second = 15.0
     supply_current_limit: units.ampere = 40.0
 
 
