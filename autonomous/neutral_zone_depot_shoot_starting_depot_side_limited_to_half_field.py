1c9bc12b472703a3481dfce3db77ff31e865326b
KhushiJain14908 <khushijain14908@gmail.com>
2026-03-28 16:01:35 -0700

    added auton for nz+depot intake/shoot that is limited to half field, and increased intake speed during outpost/nz auton

---
+++ autonomous/neutral_zone_depot_shoot_starting_depot_side_limited_to_half_field.py
@@ -0,0 +1,16 @@
+from autonomous import auto_base
+
+
+class NeutralZoneDepotShootStartingDepotSideLimitedHalfField(auto_base.AutoBase):
+    """
+    1. Start on the depot-side bump.
+    2. Drive over bump into the neutral zone, do 2 intake sweeps.
+    3. Drive over bump back into alliance zone, go to depot. Begin shooting.
+    4. Do 1 intake sweep over depot (outward).
+    5. Drive towards hub, allowing for time to finish shooting fuel.
+    6. Drive over bump back into the neutral zone.
+    """
+
+    MODE_NAME = "Neutral Zone and Depot Intake Shoot Starting Depot Side, Limited to Half Field"
+    TRAJECTORY_NAME = "neutral_zone_depot_shoot_starting_depot_side_limited_to_half_field"
+    SHOOT_ON_THE_MOVE = True
